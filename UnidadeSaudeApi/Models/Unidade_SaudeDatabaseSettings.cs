8d3cb57d09eaf8a7a95601083a1f8adaf6ee7080
Randolfo Machado <talktorandolph@live.com>
2019-09-04 19:11:51 -0300

    Criacao da entidade Unidade_SaudeDatabaseSettings na pasta Models

---
+++ UnidadeSaudeApi/Models/Unidade_SaudeDatabaseSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnidadeSaudeApi.Models
+{
+    public class Unidade_SaudeDatabaseSettings : IUnidade_SaudeDatabaseSettings
+    {
+        public string UnidadesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+    public interface IUnidade_SaudeDatabaseSettings
+    {
+        string UnidadesCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
