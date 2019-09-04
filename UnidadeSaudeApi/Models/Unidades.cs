26835f00299f934e91fd62a21b19367693a5d011
Randolfo Machado <talktorandolph@live.com>
2019-09-04 18:51:58 -0300

    Criacao da pasta Models, entidade Unidade e instacao do pacote do driver do mongodb

---
+++ UnidadeSaudeApi/Models/Unidades.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace UnidadeSaudeApi.Models
+{
+    public class Unidades
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Empreendimento")]
+        public string Empreendimento { get; set; }
+
+        [BsonElement("Subeixo")]
+        public string Subeixo { get; set; }
+
+        [BsonElement("Orgao_Responsavel")]
+        public string Orgao_Responsavel { get; set; }
+
+        [BsonElement("Executor")]
+        public string Executor { get; set; }
+
+        [BsonElement("Unidade_Federativa")]
+        public string Unidade_Federativa { get; set; }
+
+        [BsonElement("Municipio")]
+        public string Municipio { get; set; }
+
+        [BsonElement("Estagio")]
+        public string Estagio { get; set; }
+    }
+}
